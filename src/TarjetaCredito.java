94d8cf920560800ee0115d653f8028e6355a577f
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2024-05-21 20:43:29 -0600

    2024-05-21 Modelando las clases de la aplicación

---
+++ src/TarjetaCredito.java
@@ -0,0 +1,38 @@
+import src.Compra;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TarjetaCredito {
+
+    private double limite;
+    private double saldo;
+    private List<Compra> listaCompras;
+
+    public boolean lanzarCompra (Compra compra) {
+        if (this.saldo > compra.getValor()) {
+            this.saldo -= compra.getValor();
+            this.listaCompras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+    public TarjetaCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.listaCompras = new ArrayList<>();
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getListaCompras() {
+        return listaCompras;
+    }
+}
